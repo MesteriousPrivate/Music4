78684be02a5c7a692378549b4762dbd90c137509
Nand72820 <duhgfhfdghg@gmail.com>
2024-12-28 04:25:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Nand72820/Rdp",
+    "https://github.com/mycreation6666/Nand-Yaduwanshi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
