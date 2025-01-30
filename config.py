a23366e071083dfb7bdb6c030a64ee9c1a1a8567
NAND7095 <duhgfhfdghg@gmail.com>
2025-01-30 02:16:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mycreation6666/Nand-Yaduwanshi",
+    "https://github.com/YoMysterious/Music-Bots",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
