f5bb32f78927b8a3fb94cd6dd6cd5b321c116073
NAND7095 <duhgfhfdghg@gmail.com>
2025-02-02 21:06:46 +0000

    Update gali.py

--- ChampuXMusic/plugins/tools/gali.py
+++ ChampuXMusic/plugins/tools/gali.py
@@ -144,7 +144,7 @@ async def help(client: Client, message: Message):
 )
 async def help(client: Client, message: Message):
     await message.reply_text(
-        text=random.choice(GALI),
+        "**𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐈𝐬 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐃𝐦, 𝐆𝐨 𝐓𝐨 𝐁𝐨𝐭 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐀𝐧𝐝 𝐓𝐲𝐩𝐞 /gali 𝐂𝐨𝐦𝐦𝐚𝐧𝐝.**"
     )
 
 __MODULE__ = "Gᴀʟɪ"
