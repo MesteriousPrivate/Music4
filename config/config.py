db3faf46dcb1c48ca3539073ea40c0f45439cebb
Yo MesteriousPrivate <fmdevil2@gmail.com>
2025-03-04 13:48:31 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -33,11 +33,11 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/YoMysterious/Music-Bots",
+    "https://github.com/powerfulboys123/powerfulboys123",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/CreativeYdv")
