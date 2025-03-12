6521702b75824eb113106a5ae618fbca7ccdad87
Yo MesteriousPrivate <fmdevil2@gmail.com>
2025-03-12 07:32:46 +0530

    Update bot_add.py

--- ChampuXMusic/plugins/bot/bot_add.py
+++ ChampuXMusic/plugins/bot/bot_add.py
@@ -8,11 +8,13 @@ from ChampuXMusic import app
 from ChampuXMusic.utils.database import add_served_chat, get_assistant
 
 photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://files.catbox.moe/f95xbu.jpg"
+    "https://files.catbox.moe/hdb51t.jpg"
+    "https://files.catbox.moe/7lxfnr.jpg"
+    "https://files.catbox.moe/0kgyjp.jpg"
+    "https://files.catbox.moe/jr829k.jpg"
+    "https://files.catbox.moe/uuy093.jpg"
+    "https://files.catbox.moe/c0aamp.jpg"
 ]
 
 
